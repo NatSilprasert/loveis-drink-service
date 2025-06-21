4dc6751a6dbd478b8037180c4f58d570bb0f12d5
Nat Silprasert <natsilp@Nats-MacBook-Pro.local>
2025-06-21 22:59:56 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,11 +13,7 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="en">
         <body>
