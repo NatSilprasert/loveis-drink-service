c2149b23053a3322f5cf49bdc7694d78e2ccadf0
Nat Silprasert <natsilprasert@gmail.com>
2025-06-26 21:17:11 +0700

    close eslint

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['scontent.fbkk28-1.fna.fbcdn.net','res.cloudinary.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
